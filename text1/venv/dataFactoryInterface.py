b664276f48cd33d72274e9eeb30e61a8524e79cb
15024 <1502424710@qq.com>
2022-05-03 17:42:03 +0800

    注释

---
+++ text1/venv/dataFactoryInterface.py
@@ -0,0 +1,7 @@
+class dataFactoryInterface(object):
+    def __init__(self):
+        self.__name = "dataFactoryInterface"
+
+    def create(self, target):
+        classname = target + "Sampling"
+        return eval(classname)()
